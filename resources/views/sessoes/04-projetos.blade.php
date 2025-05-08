4d41577127f9809e56218e34ea255cd6b77300aa
José Vitor <Josevitornascimentolopes@gmail.com>
2025-05-08 20:40:44 -0300

    feat: adicionado sistema de projetos

---
+++ resources/views/sessoes/04-projetos.blade.php
@@ -0,0 +1,135 @@
+  <section class="projects-section">
+    <h2 class="display-4">Meus <strong class="text-gradient">Projetos</strong></h2>
+    <div class="header-divider"></div>
+    
+    <div class="projects-grid">
+      <!-- Projeto 1 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/agência de marketing.png')}}" alt="Agência de Marketing">
+        <div class="content">
+          <h3>Agência de Marketing</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>Gulp</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Projeto 2 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/Saúde.png')}}" alt="Bem Estar">
+        <div class="content">
+          <h3>Bem Estar</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>Gulp</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Projeto 3 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/anúncio de eventos.png')}}" alt="Anúncio de Eventos">
+        <div class="content">
+          <h3>Anúncio de Eventos</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>Gulp</li>
+            <li>PHP</li>
+            <li>Laravel 11</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Projeto 4 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/clone da harley davidson.png')}}" alt="Clone da Harley Davidson">
+        <div class="content">
+          <h3>Clone da Harley Davidson</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Gulp</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Projeto 5 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/clone do ifood.png')}}" alt="Clone do iFood">
+        <div class="content">
+          <h3>Clone do iFood</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>            
+            <li>Gulp</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Projeto 6 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/Concessionária.png')}}" alt="Concessionária">
+        <div class="content">
+          <h3>Concessionária</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li> 
+            <li>PHP</li>           
+            <li>Gulp</li>
+          </ul>
+        </div>
+      </div>
+
+        <!-- Projeto 7 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/imoveis.png')}}" alt="Imóveis">
+        <div class="content">
+          <h3>Imóveis</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Gulp</li>
+          </ul>
+        </div>
+      </div>
+
+        <!-- Projeto 7 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/Odonto.png')}}" alt="Odontologia">
+        <div class="content">
+          <h3>Odontologia</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Javascript</li>
+            <li>Gulp</li>
+          </ul>
+        </div>
+      </div>
+
+        <!-- Projeto 7 -->
+      <div class="project-card">
+        <img src="{{asset('img/projetos/agência de moda.png')}}" alt="Agência de Moda">
+        <div class="content">
+          <h3>Agência de Moda</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Javascript</li>
+            <li>Gulp</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+      <h4>E muito mais.</h4>
+  </section>
