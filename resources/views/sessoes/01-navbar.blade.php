d025619f28f6a7e7c67c681bfe5094e51b09a55d
José Vitor <Josevitornascimentolopes@gmail.com>
2025-05-10 04:57:52 -0300

    style: removido link para fazer registro

--- resources/views/sessoes/01-navbar.blade.php
+++ resources/views/sessoes/01-navbar.blade.php
@@ -15,7 +15,7 @@
         </div>
         <nav class="header__nav">
             <ul class="header__nav-list">
-                <li class="header__nav-item"><a href="#inicio" class="header__nav-link" id="item1">Início</a></li>
+                <li class="header__nav-item"><a href="#inicio" class="header__nav-link selected" id="item1">Início</a></li>
                 <li class="header__nav-item"><a href="#sobre" class="header__nav-link" id="item2">Sobre</a></li>
                 <li class="header__nav-item"><a href="#projetos" class="header__nav-link" id="item3">Projetos</a></li>
                 <li class="header__nav-item"><a href="#depoimentos" class="header__nav-link" id="item4">Depoimentos</a></li>
@@ -23,7 +23,7 @@
             </ul>
         </nav>
         <div class="header__actions" id="hidden">
-            <a href="#registro" class="header__action header__action--register">Registre-se</a>
+            {{-- <a href="#registro" class="header__action header__action--register">Registre-se</a> --}}
             <button class="header__action header__action--dark-mode" aria-label="Alternar modo escuro">
               <i class="fa-solid fa-sun"></i>
             </button>
