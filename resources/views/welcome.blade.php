5570b0d6f44a64ff8625c087f1a7dc36a6ab7428
José Vitor <Josevitornascimentolopes@gmail.com>
2025-05-12 16:50:50 -0300

    ajustes no SEO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
           "@type": "Organization",
           "name": "Start to Finish",
           "url": "https://www.starttofinish.com.br",
-          "logo": "https://www.starttofinish.com.br/img/icone.ico",
+          "logo": "https://www.starttofinish.com.br/public/img/icone.ico",
           "description": "A 'Start to Finish' oferece soluções completas em tecnologia, sites, sistemas e aplicações web personalizadas. Inove seu projeto com agilidade e qualidade.",
           "address": {
             "@type": "PostalAddress",
