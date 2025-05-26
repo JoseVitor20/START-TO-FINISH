8543fcc2668c9c79514d08a9feda59158e80583e
José Vitor <Josevitornascimentolopes@gmail.com>
2025-05-26 15:16:32 -0300

    style: alterado os estilos da sessão projetos

--- resources/views/sessoes/04-projetos.blade.php
+++ resources/views/sessoes/04-projetos.blade.php
@@ -2,8 +2,102 @@
     <div class="container">
         <h2 class="display-4 fw-bold">Explore por <strong class="text-gradient">Categorias</strong></h2>
         <div class="header-divider"></div>
-        <div class="categorias-container">
-            <!-- Categorias serão adicionadas via JavaScript -->
+        <div class="categorias-container"><div class="categoria-card">
+            <a href="restaurantes">
+                <div class="categoria-single">
+                        <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" alt="Restaurantes" class="categoria-icon">
+                        <div class="categoria-info">
+                            <h3 class="categoria-nome">Restaurantes</h3>
+                            <p class="categoria-desc">Encontre layouts para Restaurantes</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="mercados">                
+                <div class="categoria-card">
+                <div class="categoria-single">
+                    <img src="https://images.unsplash.com/photo-1578916171728-46686eac8d58?w=600&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1lcmNhZG98ZW58MHx8MHx8fDA%3D" alt="Mercados" class="categoria-icon">
+                    <div class="categoria-info">
+                        <h3 class="categoria-nome">Mercados</h3>
+                        <p class="categoria-desc">Encontre layouts para Mercado</p>
+                    </div>
+                </div>
+                </div>
+            </a>
+            
+            <a href="panificadoras">                
+                <div class="categoria-card">
+                <div class="categoria-single">
+                    <img src="https://images.unsplash.com/photo-1509440159596-0249088772ff?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" alt="Panificadoras" class="categoria-icon">
+                    <div class="categoria-info">
+                        <h3 class="categoria-nome">Panificadoras</h3>
+                        <p class="categoria-desc">Encontre layouts para Panificadoras</p>
+                    </div>
+                </div>
+                </div>
+            </a>
+            
+            <a href="salao-beleza">                
+                <div class="categoria-card">
+                <div class="categoria-single">
+                    <img src="https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" alt="Salões de Beleza" class="categoria-icon">
+                    <div class="categoria-info">
+                        <h3 class="categoria-nome">Salões de Beleza</h3>
+                        <p class="categoria-desc">Encontre layouts para Salões de Beleza</p>
+                    </div>
+                </div>
+                </div>
+            </a>
+            
+            <a href="farmacias">                
+                <div class="categoria-card">
+                <div class="categoria-single">
+                    <img src="https://images.unsplash.com/photo-1584308666744-24d5c474f2ae?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" alt="Farmácias" class="categoria-icon">
+                    <div class="categoria-info">
+                        <h3 class="categoria-nome">Farmácias</h3>
+                        <p class="categoria-desc">Encontre layouts para Farmácias</p>
+                    </div>
+                </div>
+                </div>
+            </a>
+            
+            <a href="lojas-roupas">                
+                <div class="categoria-card">
+                <div class="categoria-single">
+                    <img src="https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" alt="Lojas de Roupas" class="categoria-icon">
+                    <div class="categoria-info">
+                        <h3 class="categoria-nome">Lojas de Roupas</h3>
+                        <p class="categoria-desc">Encontre layouts para Lojas de Roupas</p>
+                    </div>
+                </div>
+                </div>
+            </a>
+            
+            <a href="psicologico">                
+                <div class="categoria-card">
+                <div class="categoria-single">
+                    <img src="https://media.istockphoto.com/id/2174235368/pt/foto/in-therapists-office-woman-shares-and-counselor-takes-notes.webp?a=1&amp;b=1&amp;s=612x612&amp;w=0&amp;k=20&amp;c=3s9YazVALzgH0xrzolEoLCCRrj3bqt2zagY4-AghVd4=" alt="Consultório Psicológico" class="categoria-icon">
+                    <div class="categoria-info">
+                        <h3 class="categoria-nome">Consultório Psicológico</h3>
+                        <p class="categoria-desc">Encontre layouts para Consultório Psicológico</p>
+                    </div>
+                </div>
+                </div>
+            </a>
+            
+            <a href="lojas-moveis">                
+                <div class="categoria-card">
+                <div class="categoria-single">
+                    <img src="https://media.istockphoto.com/id/2191392441/pt/foto/kitchen-showroom-exploration-with-modern-island-features.webp?a=1&amp;b=1&amp;s=612x612&amp;w=0&amp;k=20&amp;c=YdTWncFsm5aY8KfOdM0anh0MBAwugNBAMabCNRR_umQ=" alt="Lojas de móveis" class="categoria-icon">
+                    <div class="categoria-info">
+                        <h3 class="categoria-nome">Lojas de móveis</h3>
+                        <p class="categoria-desc">Encontre layouts para Lojas de móveis</p>
+                    </div>
+                </div>
+                </div>
+            </a>
+            
         </div>
     </div>
 </section>
