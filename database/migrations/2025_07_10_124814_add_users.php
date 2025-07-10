ce0bced62596c899f88e1d77a51b80ba46b4824f
José Vitor <Josevitornascimentolopes@gmail.com>
2025-07-10 09:58:59 -0300

    Deploy para adicionar nova coluna

--- database/migrations/2025_07_10_124814_add_users.php
+++ database/migrations/2025_07_10_124814_add_users.php
@@ -23,6 +23,6 @@ return new class extends Migration
     {
         Schema::table('users', function(Blueprint $table){
             $table->string('role')->default('client');
-        })
+        });
     }
 };
